82208b27d4f2fe0adaa5e89d2e5a9f6bce23ba4c
Christopher Manzano Vimos <cmanzano@student.42madrid.com>
2021-11-23 10:03:47 +0100

    llol

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: cmanzano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 11:19:29 by cmanzano          #+#    #+#             */
-/*   Updated: 2021/11/22 13:29:35 by cmanzano         ###   ########.fr       */
+/*   Updated: 2021/11/23 10:02:12 by cmanzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,11 @@ char		*ft_strtrim(char const *s1, char const *set)
 		return (ft_strdup(s1));
 	end = get_end((char *) s1, set);
 	start =	 get_start((char *) s1, set);
-	printf("start (%u), end (%u)", start, end);
-	n = (char *) ft_calloc(end - start + 1, sizeof(char));
+	if (end < start)
+		return ft_strdup("");
+	n = (char *) ft_calloc(end - start + 2, sizeof(char));
+	if (!n)
+		return (n);
 	ft_memcpy(n, s1 + start, end - start + 1);
 	n[end - start + 1] = 0;
 	return (n);
@@ -65,7 +68,10 @@ static unsigned int	get_end(char *s1, char const *set)
 	unsigned int	j;
 	int				finished;
 
-	end = ft_strlen(s1) - 1;
+	end = ft_strlen(s1);
+	if (!end)
+		return (end);
+	end--;
 	i = end;
 	finished = 0;
 	while (i > 0 && !finished && end > 0)
@@ -73,8 +79,6 @@ static unsigned int	get_end(char *s1, char const *set)
 		j = 0;
 		while (s1[i] != set[j] && set[j])
 		{
-			printf("-------------\n");
-			printf("char (%c) str (%c)\n", set[j], s1[i]);
 			j++;
 		}
 		if (set[j] != s1[i])
