9501066434922740781a2daed5984834d2c185b8
Christopher Manzano Vimos <cmanzano@student.42madrid.com>
2021-12-04 19:14:41 +0100

    optimizig lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: cmanzano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 17:55:32 by cmanzano          #+#    #+#             */
-/*   Updated: 2021/12/04 18:23:01 by cmanzano         ###   ########.fr       */
+/*   Updated: 2021/12/04 19:04:49 by cmanzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	if (*lst != 0)
-	{
+	void	*ptr;
+
 		if ((*lst) -> next == 0)
 		{
 			ft_lstdelone(*lst, del);
 		}
 		else
-		{
-			ft_lstclear(&((*lst) -> next), del);
-			ft_lstdelone((*lst) -> next, del);
+		{	
+			ptr = &((*lst) -> next);
+			ft_lstdelone((*lst), del);
+			ft_lstclear(ptr, del);
 		}
 		*lst = 0;
-	}
 }
